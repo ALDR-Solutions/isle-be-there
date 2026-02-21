8e5190b7c94f39c9f1cb4f2684b3326d2cb9eee6
Jabezgrif <danielgrif16@gmail.com>
2026-02-21 10:35:28 +0800

    feat: Add listings and bookings views to the main views list

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -4,8 +4,10 @@ from .user import user_views
 from .index import index_views
 from .auth import auth_views
 from .account import account_views
+from .listings import listings_view
+from .bookings import bookings_view
 # from .admin import setup_admin
 
 
-views = [user_views, index_views, auth_views, account_views] 
+views = [user_views, index_views, auth_views, account_views, listings_view, bookings_view] 
 # blueprints must be added to this list
