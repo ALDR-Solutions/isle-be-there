a8ff43b4129e4ff8ad166a78adeba2652552ca15
Jabezgrif <danielgrif16@gmail.com>
2026-02-21 12:30:42 +0800

    feat: Add business view blueprint to the views list

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -6,8 +6,9 @@ from .auth import auth_views
 from .account import account_views
 from .listings import listings_view
 from .bookings import bookings_view
+from .business import business_view
 # from .admin import setup_admin
 
 
-views = [user_views, index_views, auth_views, account_views, listings_view, bookings_view] 
+views = [user_views, index_views, auth_views, account_views, listings_view, bookings_view, business_view]  # add new blueprints here
 # blueprints must be added to this list
