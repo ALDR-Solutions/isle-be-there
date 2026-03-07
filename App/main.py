2b365b8972e05402d60e393be5fa3b2fccdd03f1
Jabezgrif <danielgrif16@gmail.com>
2026-03-07 10:09:30 +0800

    Refactor code structure for improved readability and maintainability

--- App/main.py
+++ App/main.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask, render_template
-# from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
+from flask_uploads import DOCUMENTS, IMAGES, TEXT, UploadSet, configure_uploads
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
-from flask_bootstrap import Bootstrap5
 
 from App.database import init_db
 from App.config import load_config
@@ -15,8 +14,7 @@ from App.controllers import (
     add_auth_context
 )
 
-from App.views import views
-from App.controllers.auth import get_business_profile, get_current_user, get_user_profile
+from App.views import views, setup_admin
 
 
 
@@ -29,29 +27,15 @@ def create_app(overrides={}):
     load_config(app, overrides)
     CORS(app)
     add_auth_context(app)
-    # photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
-    # configure_uploads(app, photos)
+    photos = UploadSet('photos', TEXT + DOCUMENTS + IMAGES)
+    configure_uploads(app, photos)
     add_views(app)
     init_db(app)
-    @app.context_processor
-    def inject_current_user():
-        user = get_current_user()  # call the function to get the actual user
-        if user and user.user_metadata.get('is_business'):
-            user_profile = get_business_profile(user)
-        else:
-            user_profile = get_user_profile(user) if user else None
-        return {
-            "current_user": user,
-            "user_profile": user_profile
-        }
-    Bootstrap5(app)
     jwt = setup_jwt(app)
-    # setup_admin(app)
+    setup_admin(app)
     @jwt.invalid_token_loader
     @jwt.unauthorized_loader
     def custom_unauthorized_response(error):
         return render_template('401.html', error=error), 401
     app.app_context().push()
-    
-    
     return app
--- App/views/__init__.py
+++ App/views/__init__.py
@@ -3,12 +3,9 @@
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
-from .account import account_views
-from .listings import listings_view
-from .bookings import bookings_view
-from .business import business_view
-# from .admin import setup_admin
+from .onboarding import onboarding_views
+from .admin import setup_admin
 
 
-views = [user_views, index_views, auth_views, account_views, listings_view, bookings_view, business_view]  # add new blueprints here
+views = [user_views, index_views, auth_views, onboarding_views]
 # blueprints must be added to this list
