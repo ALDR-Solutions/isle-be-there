99d8bd5de1fc53ea77a74638568baad421247b7d
Aidan Newallo <aidannewallo@gmail.com>
2026-04-01 09:29:00 +0800

    refactor: add initial Alembic configuration and migration scripts

---
+++ backend/alembic/versions/001_initial_schema.py
@@ -0,0 +1,28 @@
+"""initial schema
+
+Revision ID: 001_initial_schema
+Revises: 
+Create Date: 2026-04-01 08:48:51.321528
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '001_initial_schema'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
