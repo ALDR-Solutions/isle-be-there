d52719d8d823655808ddea618d83c69c5d74bfa0
Aidan Newallo <aidannewallo@gmail.com>
2026-05-06 12:16:33 +0800

    feat: add discount and pricing tables, update itineraries and bookings for new fields

--- backend/alembic/versions/d48264722ede_revert_table.py
+++ backend/alembic/versions/d48264722ede_revert_table.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'd48264722ede'
-down_revision: Union[str, Sequence[str], None] = '168a45956f2f'
+down_revision: Union[str, Sequence[str], None] = 'c82f3079f515'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
